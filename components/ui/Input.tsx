573fe8e99a9fcfb59735d361274b68bb16ccb6fd
omash735-sudo <omash735@gmail.com>
2026-08-27 23:54:53 +0200

    Create Input.tsx

---
+++ components/ui/Input.tsx
@@ -0,0 +1,32 @@
+// components/ui/Input.tsx
+import { forwardRef } from 'react';
+import { cn } from '@/lib/utils';
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  error?: string;
+}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, error, ...props }, ref) => {
+    return (
+      <div className="w-full">
+        <input
+          ref={ref}
+          className={cn(
+            'input-field',
+            error && 'border-red-500 focus:border-red-500 focus:ring-red-500/20',
+            className
+          )}
+          {...props}
+        />
+        {error && (
+          <p className="mt-1 text-sm text-red-600">{error}</p>
+        )}
+      </div>
+    );
+  }
+);
+
+Input.displayName = 'Input';
+
+export { Input };
