5c9b6721cb5f363606ecf76de2f63969907c0310
omash735-sudo <omash735@gmail.com>
2026-08-28 03:45:54 +0200

    Update index.ts

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -4,5 +4,5 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
 const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql, { schema });
+export const db = drizzle(sql, { schema, logger: true });
 export * from './schema';
