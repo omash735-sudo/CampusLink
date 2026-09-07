c50a303917f7caae446365ad09f13435c34e7545
omash735-sudo <omash735@gmail.com>
2026-09-07 14:54:16 +0200

    Update index.ts

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -3,9 +3,6 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
-const connectionString = process.env.DATABASE_URL!;
-const sql = neon(connectionString);
-
-// Cast to any to bypass type checking
-export const db = drizzle(sql as any, { schema });
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema });
 export * from './schema';
