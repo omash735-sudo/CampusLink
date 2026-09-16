eb672f62ebf08a03707bd38bcb99d6f44b5fe9e1
omash735-sudo <omash735@gmail.com>
2026-09-16 17:57:25 +0200

    Update ResourceGrid.tsx

--- components/resources/ResourceGrid.tsx
+++ components/resources/ResourceGrid.tsx
@@ -3,10 +3,9 @@ import { ResourceCard } from './ResourceCard';
 
 interface ResourceGridProps {
   resources: any[];
-  currentUserId?: string;
 }
 
-export function ResourceGrid({ resources, currentUserId }: ResourceGridProps) {
+export function ResourceGrid({ resources }: ResourceGridProps) {
   if (resources.length === 0) {
     return (
       <div className="border border-gray-200 bg-white p-8 text-center">
@@ -18,11 +17,7 @@ export function ResourceGrid({ resources, currentUserId }: ResourceGridProps) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {resources.map((resource) => (
-        <ResourceCard
-          key={resource.id}
-          resource={resource}
-          currentUserId={currentUserId}
-        />
+        <ResourceCard key={resource.id} resource={resource} />
       ))}
     </div>
   );
