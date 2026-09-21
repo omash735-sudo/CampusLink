6665ab3aaf53283745a4836e1cb04bf3aca1d03c
omash735-sudo <omash735@gmail.com>
2026-09-21 12:41:53 +0200

    Fix syntax error in middleware.ts config export

--- middleware.ts
+++ middleware.ts
@@ -157,6 +157,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/(((?!_next/static|_next/image|favicon.ico|manifest.json|service-worker.js|icons/|images/|api/).*)',
+    '/((?!_next/static|_next/image|favicon.ico|manifest.json|service-worker.js|icons/|images/|api/).*)',
   ],
 };
