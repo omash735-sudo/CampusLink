564f4cb0a6d7edfb07844d6dad74f6efdf1122ae
omash735-sudo <omash735@gmail.com>
2026-09-24 17:38:40 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'CampusLink | Connect. Discover. Belong.',
-  description: 'Your digital community for LUANAR City Campus',
+  description: 'Your digital community for Mw Campuses',
   icons: {
     icon: 'https://res.cloudinary.com/dfsvnaslv/image/upload/v1788726475/icon-mark-transparent_qnuzur.png',
     apple: 'https://res.cloudinary.com/dfsvnaslv/image/upload/v1788726475/icon-mark-transparent_qnuzur.png',
